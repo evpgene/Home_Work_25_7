de04a8e5779caef16763d220280dccc940c14c1c
evpgene <evpgene@mail.ru>
2023-07-07 01:41:41 +0500

    Вроде работают все? запросы

--- Chat_server/DB_Queries.h
+++ Chat_server/DB_Queries.h
@@ -4,35 +4,11 @@
 #include <cstring>
 #include <iostream>
 #include <utility>
-
 #include "Chat.h"
 #include "User.h"
 
 #define STRING_SIZE 255
-
-#define INSERT_USER "INSERT INTO users (login, pass) VALUES (?,?)"
-
-#define SELECT_USER "SELECT id, login, pass FROM users WHERE id = ?"
-
-#define INSERT_CHAT "INSERT INTO chats (name) VALUES (?)"
-
-#define SELECT_CHAT "SELECT id, name FROM chats WHERE id = ?"
-
-#define INSERT_CHAT_USER \
-  "INSERT INTO chat_user (chat_id, user_id, user_no) VALUES (?,?,?)"
-
-#define SELECT_CHAT_USER \
-  "SELECT id FROM chat_user WHERE chat_id = ? AND user_id = ?"
-
-// insert message & select message можно перенести в отдельный класс для
-// одиночного чата, чтобы в последствии исполнялся в отдельном потоке с
-// отдельной коннекцией ???
-
-#define INSERT_MESSAGE                                       \
-  "INSERT INTO messages (chat_user_id, message, dt) VALUES " \
-  "(?,?,CURRENT_TIMESTAMP())"
-
-#define SELECT_MESSAGE "SELECT * FROM message_view WHERE chat_id = ? AND message_id = ?"
+using queue_message_t = std::shared_ptr<std::queue<Message>>;
 
 struct ChatUser {
   ChatUser(){};
@@ -43,21 +19,27 @@ struct ChatUser {
   int user_id{-1};
 };
 
-//!!!!!!!!!!!!!!!!!!    mysql_stmt_insert_id()
-
 class DB_Queries {
  private:
-  // MYSQL msql;
-
   MYSQL* mysql = new MYSQL;
   MYSQL_RES* res;
   MYSQL_ROW row;
-
   struct String_Param {
     char str_data[STRING_SIZE];
     unsigned long str_length{STRING_SIZE};
   };
+  struct Int_Param {
+    int int_data{-1};
+  };
 
+ public:
+  DB_Queries(/* args */);
+  ~DB_Queries();
+
+  void connectDB_open(void);
+  void connectDB_close(void);
+
+#define INSERT_USER "INSERT INTO users (login, pass) VALUES (?,?)"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -68,7 +50,11 @@ class DB_Queries {
       int param_count;
     } Query;
   } Insert_User;
+  void insertUser_prepare(void);
+  size_t insertUser(const User_t user);
+  int insertUser_close(void);
 
+#define SELECT_USER "SELECT id, login, pass FROM users WHERE id = ?"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -89,25 +75,33 @@ class DB_Queries {
       bool error[3];
     } Result;
   } Select_User;
+  void selectUser_prepare(void);
+  User_t selectUser(const int id);
+  void selectUser_close(void);
 
+#define INSERT_CHAT "INSERT INTO chats (name) VALUES (?)"
   struct {
-    struct {
+  struct {
       MYSQL_STMT* stmt;
       MYSQL_BIND bind[1];
       uint64_t affected_rows;
       String_Param Name;
       int param_count;
-    } Query;
+  } Query;
   } Insert_Chat;
+  void insertChat_prepare(void);
+  size_t insertChat(const Chat_t chat);
+  int insertChat_close(void);
 
+#define SELECT_CHAT "SELECT id, name FROM chats WHERE id = ?"
   struct {
-    struct {
+  struct {
       MYSQL_STMT* stmt;
       MYSQL_BIND bind[1];
       int param_count;
       int int_data;
-    } Query;
-    struct {
+  } Query;
+  struct {
       MYSQL_BIND bind[1];
       MYSQL_RES* prepare_meta_result;
       String_Param Name;
@@ -117,16 +111,14 @@ class DB_Queries {
       int int_data;
       bool is_null[1];
       bool error[1];
-    } Result;
+  } Result;
   } Select_Chat;
+  void selectChat_prepare(void);
+  Chat_t selectChat(const int id);
+  void selectChat_close(void);
 
-
-struct Int_Param {
-  int int_data {-1};
-};
-
-
-
+#define INSERT_CHAT_USER \
+  "INSERT INTO chat_user (chat_id, user_id, user_no) VALUES (?,?,?)"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -137,8 +129,12 @@ struct Int_Param {
       int param_count;
     } Query;
   } Insert_ChatUser;
+  void insertChatUser_prepare(void);
+  size_t insertChatUser(const int chat_id, const int user_id);
+  int insertChatUser_close(void);
 
-
+#define SELECT_CHAT_USER \
+  "SELECT id FROM chat_user WHERE chat_id = ? AND user_id = ?"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -159,119 +155,13 @@ struct Int_Param {
       bool error[1];
     } Result;
   } Select_ChatUser;
-
-
-
-
- public:
-  DB_Queries(/* args */);
-  ~DB_Queries();
-
-  // шаг разворачивание базы данных
-  // create database
-  // create database user
-  // create tables
-
-  // шаг восстановление чата из базы данных
-  // retrieve Users
-  // retrieve Chats
-  // retrieve Messages
-
-  // шаг добавление сообщений
-
-  User_t retrieveUser(size_t user_id);
-  Chat_t retrieveChat(size_t chat_id);
-  void retrieveUsers(std::vector<User_t>& users);
-  void retrieveChats(std::vector<Chat_t>& chats);
-
-  void connectDB_open(void);
-  void connectDB_close(void);
-
-
-  void insertUser_prepare(void);
-  size_t insertUser(const User_t user);
-  int insertUser_close(void);
-
-  void selectUser_prepare(void);
-  User_t selectUser(const int id);
-  void selectUser_close(void);
-
-
-  void insertChat_prepare(void);
-  size_t insertChat(const Chat_t chat);
-  int insertChat_close(void);
-
-  void selectChat_prepare(void);
-  Chat_t selectChat(const int id);
-  void selectChat_close(void);
-
-
-  void insertChatUser_prepare(void);
-  size_t insertChatUser(const int chat_id, const int user_id);
-  int insertChatUser_close(void);
-
   void selectChatUser_prepare(void);
   size_t selectChatUser(const int chat_id, const int user_id);
   void selectChatUser_close(void);
 
-
-
-
-
-
-
-  void insertMessage_prepare(void);
-  size_t insertMessage(const int chat_user_id, const Message_t message);
-  int insertMessage_close(void);
-
-  void selectMessage_prepare(void);
-  Message_t selectMessage(const int chat_id, const int message_id);
-  void selectMessage_close(void);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #define INSERT_MESSAGE                                       \
-//   "INSERT INTO messages (chat_user_id, message, dt) VALUES " \
-//   "(?,?,CURRENT_TIMESTAMP())"
-
-// CREATE TABLE IF NOT EXISTS messages (
-//   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-//   chat_user_id INT UNSIGNED NOT NULL REFERENCES chats_user(id),
-//   message character varying(255) NOT NULL,
-//   dt DATETIME NOT NULL,
-//   st INT,
-//   PRIMARY KEY (id),
-//   CONSTRAINT chat_user_id_fk FOREIGN KEY(chat_user_id) REFERENCES chat_user(id) 
-// )AUTO_INCREMENT = 1;
-
-// ALTER VIEW message_view AS
-// SELECT chats.id AS chat_id, chats.name AS chatname , users.login AS login, messages.dt AS timesend, messages.message AS message   FROM messages 
-// JOIN chat_user ON chat_user.id = messages.chat_user_id
-// JOIN chats ON chats.id = chat_user.chat_id
-// JOIN users ON users.id = chat_user.user_id
-
+#define INSERT_MESSAGE                                       \
+  "INSERT INTO messages (chat_user_id, message, dt) VALUES " \
+  "(?,?,CURRENT_TIMESTAMP())"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -282,7 +172,12 @@ struct Int_Param {
       int param_count;
     } Query;
   } Insert_Message;
+  void insertMessage_prepare(void);
+  size_t insertMessage(const int chat_user_id, const Message_t message);
+  int insertMessage_close(void);
 
+#define SELECT_MESSAGE \
+  "SELECT * FROM message_view WHERE chat_id = ? AND message_id = ?"
   struct {
     struct {
       MYSQL_STMT* stmt;
@@ -292,28 +187,97 @@ struct Int_Param {
       int param_count;
     } Query;
     struct {
-      MYSQL_BIND bind[5];
+      MYSQL_BIND bind[7];
       MYSQL_RES* prepare_meta_result;
       Int_Param chat_id, user_id, message_id;
       //String_Param chatname;
       MYSQL_TIME dt;
       String_Param login;
       String_Param message;
-      unsigned long length[5];
+      Int_Param status;
+      unsigned long length[7];
       int param_count, column_count, row_count;
-      bool is_null[5];
-      bool error[5];
+      bool is_null[7];
+      bool error[7];
     } Result;
   } Select_Message;
+  void selectMessage_prepare(void);
+  Message_t selectMessage(const int chat_id, const int message_id);
+  void selectMessage_close(void);
 
+#define SELECT_MESSAGES                                                        \
+  "SELECT * FROM message_view WHERE chat_id = ? AND message_id BETWEEN ? AND " \
+  "? AND status = ? LIMIT ?"
+  struct {
+    struct {
+      MYSQL_STMT* stmt;
+      MYSQL_BIND bind[5];
+      Int_Param chat_id;
+      Int_Param message_id_begin;
+      Int_Param message_id_end;
+      Int_Param status;
+      Int_Param limit;
+      int param_count;
+    } Query;
+    struct {
+      MYSQL_BIND bind[7];
+      MYSQL_RES* prepare_meta_result;
+      Int_Param chat_id, user_id, message_id;
+      //String_Param chatname;
+      MYSQL_TIME dt;
+      String_Param login;
+      String_Param message;
+      Int_Param status;
+      unsigned long length[7];
+      int param_count, column_count, row_count;
+      bool is_null[7];
+      bool error[7];
+    } Result;
+  } Select_Messages;
+  void selectMessages_prepare(void);
+  queue_message_t selectMessages(const int chat_id, const int message_id_begin,
+                                 const int message_id_end,
+                                 const int message_status, const int limit);
+  void selectMessages_close(void);
 
+#define UPDATE_STATUS_DELIVERED                                               \
+  "UPDATE messages SET status = 2, dt_delivered = CURRENT_TIMESTAMP() WHERE " \
+  "chat_user_id = ? AND id BETWEEN ? AND ? AND status != 2 AND status != 3"
+  struct {
+    struct {
+      MYSQL_STMT* stmt;
+      MYSQL_BIND bind[3];
+      uint64_t affected_rows;
+      Int_Param chat_user_id;
+      Int_Param message_id_begin;
+      Int_Param message_id_end;
+      int param_count;
+    } Query;
+  } Update_Status_Delivered;
+  void updateStatusDelivered_prepare(void);
+  size_t updateStatusDelivered(const int chat_user_id,
+                               const int message_id_begin,
+                               const int message_id_end);
+  int updateStatusDelivered_close(void);
 
-
-
-
-
-
-
-
+#define UPDATE_STATUS_READ                                               \
+  "UPDATE messages SET status = 3, dt_read = CURRENT_TIMESTAMP() WHERE " \
+  "chat_user_id = ? AND id BETWEEN ? AND ? AND status = 2"
+  struct {
+    struct {
+      MYSQL_STMT* stmt;
+      MYSQL_BIND bind[3];
+      uint64_t affected_rows;
+      Int_Param chat_user_id;
+      Int_Param message_id_begin;
+      Int_Param message_id_end;
+      int param_count;
+    } Query;
+  } Update_Status_Read;
+  void updateStatusRead_prepare(void);
+  size_t updateStatusRead(const int chat_user_id,
+                               const int message_id_begin,
+                               const int message_id_end);
+  int updateStatusRead_close(void);
 
 };
--- Chat_server/Main.cpp
+++ Chat_server/Main.cpp
@@ -6,48 +6,64 @@ using namespace std;
 int main() {
   // system("chcp 1251");
 
-  //Chats chats;
-
+  // Chats chats;
 
   DB_Queries db_queries;
   db_queries.connectDB_open();
-
   db_queries.selectUser_prepare();
   db_queries.selectChat_prepare();
   db_queries.selectChatUser_prepare();
   db_queries.selectMessage_prepare();
-
+  db_queries.insertMessage_prepare();
+  db_queries.selectMessages_prepare();
+  db_queries.updateStatusDelivered_prepare();
+  db_queries.updateStatusRead_prepare();
 
   User_t user(db_queries.selectUser(1));
   std::cout << user->getLogin() << std::endl;
   std::cout << user->getPass() << std::endl;
 
- 
   Chat_t chat(db_queries.selectChat(1));
   std::cout << chat->getChatName() << std::endl;
 
-  Message_t message(db_queries.selectMessage(1,1));
+  Message_t message = db_queries.selectMessage(1, 1);
+  message->printMessage();
   std::cout << message->getTimeSend() << std::endl;
   std::cout << message->getUserName() << std::endl;
   std::cout << message->getMessage() << std::endl;
 
-   size_t chatuser = db_queries.selectChatUser(1, 1);
+  size_t chatuser = db_queries.selectChatUser(1, 1);
   std::cout << "ChatUser ID = " << chatuser << std::endl;
 
+  // Message_t message_arr[20];
+  // message_arr[0] = std::make_shared<Message>(Message("","", "ну прювет
+  // волк"));
 
+  // db_queries.insertMessage(1, message_arr[0]);
+
+  queue_message_t queue = db_queries.selectMessages(1, 1, 5, 3, 100);
+
+  while (!queue->empty()) {
+    // Message message(queue->front());
+    queue->front().printMessage();
+    queue->pop();
+  }
+
+  db_queries.updateStatusDelivered(1, 1, 10);
+  db_queries.updateStatusRead(1, 1, 10);
 
   db_queries.selectChat_close();
   db_queries.selectUser_close();
   db_queries.selectChatUser_close();
   db_queries.selectMessage_close();
+  db_queries.insertMessage_close();
   db_queries.connectDB_close();
+  db_queries.selectMessages_close();
+  db_queries.updateStatusDelivered_close();
+  db_queries.updateStatusRead_close();
   // chats.restordata();
   // chats.mainmenu();
   // chats.savedata();
-
-
-
-
 
   return 0;
 }
